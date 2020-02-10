211f1e10d17135fc08395a4152e66c9f94860b5b
Karolina <zygawska.karolina@gmail.com>
2020-02-10 21:53:57 +0100

    wersja 10.02.20
    Zmiany
    +Zapisuje wybrane elemnty do bazy 100 (dzięki czemu nie trzeba dodwać kolumn z dlugościa i powierzchnia- same się generują)
    + selekcja obiektów, które następnie zamieniane są na punktowe
    + usuwanie zbędnych elementów
    W każdym momęcie można poszerzyć bazę o kolejne obiekty a teraz przejdę do tworzenia layout'u i dodwania symboli

--- mgr.py
+++ mgr.py
@@ -5,51 +5,68 @@ sciezka = arcpy.GetParameterAsText(0) # pobrania danych wej?ciowych
 lok_baza = arcpy.GetParameterAsText(1)
 
 #arcpy.CreateFileGDB_management("D:\mgr", "Baza100") # tworzymy baze
-dane_wyjsc = "D:\mgr\Baza100.gdb"                       # definiujemy ta baze
-#baza_sciezka = os.path.join(sciezka, "Baza.gdb") # ?
+dane_wyjsc = "D:\mgr\Baza100.gdb"                    # definiujemy ta baze
+arcpy.env.workspace = dane_wyjsc
+#Selekcja obiektow
+inFeatures = ['D:\mgr\SHP_M33035AB\Budynek_P_PAL015.shp',
+ 'D:\mgr\SHP_M33035AB\Budynek_A_AAL015.shp',
+ 'D:\mgr\SHP_M33035AB\Magazyn_skl_AAM010.shp',
+ 'D:\mgr\SHP_M33035AB\Ogrodzenie_LAL070.shp',
+ 'D:\mgr\SHP_M33035AB\Mur_ogrodze_LAL260.shp',
+ 'D:\mgr\SHP_M33035AB\Park_A_AAK120.shp',
+ 'D:\mgr\SHP_M33035AB\Stadion_amf_AAK160.shp',
+ 'D:\mgr\SHP_M33035AB\Stadion_amf_PAK160.shp']
 
+outLocation = dane_wyjsc
+#zapisuje wybrane elementy w bazie (dodawane sa tebele z wartosciami dlugosci i powierzchni obiektu)
+arcpy.FeatureClassToGeodatabase_conversion(inFeatures, outLocation)
 
-arcpy.env.workspace = "D:\mgr\SHP_M33035AB"
+# Tworzy warstwe obiektow
+arcpy.MakeFeatureLayer_management('Budynek_P_PAL015', 'Budynek_P_layer')
+arcpy.MakeFeatureLayer_management('Budynek_A_AAL015', 'Budynek_A_layer')
+arcpy.MakeFeatureLayer_management('Magazyn_skl_AAM010', 'Magazyn_skl_layer')
+arcpy.MakeFeatureLayer_management('Ogrodzenie_LAL070', 'Ogrodzenie_layer')
+arcpy.MakeFeatureLayer_management('Mur_ogrodze_LAL260', 'Mur_ogrodze_layer')
+arcpy.MakeFeatureLayer_management('Park_A_AAK120', 'Park_layer')
+arcpy.MakeFeatureLayer_management('Stadion_amf_AAK160', 'Stadion_amf_A_layer')
+arcpy.MakeFeatureLayer_management('Stadion_amf_PAK160', 'Stadion_amf_P_layer')
 
-pomnik =  "Pomnik_P_PAL130.shp"
-pas_drzew = "Pas_drzew_z_LEA020.shp"
-zakl_prze = "Zaklad_prze_AAC000.shp"
-jezioro_staw = "Jezioro_sta_ABH080.shp"
-rzeka = "Rzeka_strum_LBH140.shp"
+#Selekcjonuje obiekty ktore nastepnie sa zamieniane na punktowe
+arcpy.SelectLayerByAttribute_management('Stadion_amf_A_layer', "NEW_SELECTION",' "Shape_Area" < 15000 ')
+arcpy.FeatureToPoint_management('Stadion_amf_A_layer', 'Stadion_amf_P_layer')
 
+arcpy.SelectLayerByAttribute_management('Budynek_A_layer', "NEW_SELECTION",' "Shape_Area" < 2700 ')
+arcpy.FeatureToPoint_management('Budynek_A_layer', 'Budynek_P')
 
-
-#for fc in arcpy.ListFeatureClasses():
-    #arcpy.AddField_management(fc, "Lenght", "DOUBLE")
-    #with arcpy.da.UpdateCursor(fc, "Lenght") as cursor:
-     #   for row in cursor:
-      #  dlugosc = row[0]
-     #   cursor.updateRow(row)
-
-
-arcpy.MakeFeatureLayer_management(rzeka, 'rzeka_layer')
-
-
-#pole_wektor = ["Length"]
-#arcpy.AddField_management("rzeka_layer", pole_wektor[0], "DOUBLE")
-
-#with arcpy.da.UpdateCursor('rzeka_layer', ["SHAPE@LENGTH", "Length"]) as cursor:
-    #for row in cursor:
-        #row[1] = row[0]
-        #cursor.updateRow(row)
-
-
-arcpy.SelectLayerByAttribute_management('rzeka_layer', "NEW_SELECTION",' "Length8" > 220 ')
-
+#Selekcja po atrybutach
+arcpy.SelectLayerByAttribute_management('Budynek_A_layer', "NEW_SELECTION",' "Shape_Area" > 2700 ')
+arcpy.SelectLayerByAttribute_management('Magazyn_skl_layer', "NEW_SELECTION",' "Shape_Area" > 25000 ')
+arcpy.SelectLayerByAttribute_management('Ogrodzenie_layer', "NEW_SELECTION",' "Shape_Length" > 500 ')
+arcpy.SelectLayerByAttribute_management('Mur_ogrodze_layer', "NEW_SELECTION",' "Shape_Length" > 500 ')
+arcpy.SelectLayerByAttribute_management('Park_layer', "NEW_SELECTION",' "Shape_Area" > 15000 ')
+arcpy.SelectLayerByAttribute_management('Stadion_amf_A_layer', "NEW_SELECTION",' "Shape_Area" > 15000 ')
+#Definiuje uk?ad wspolrzednych
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("ETRS 1989 UTM Zone 33N")
 
-#arcpy.FeatureClassToFeatureClass_conversion('rzeka_layer', dane_wyjsc, 'Rzeka_strum_L')
+#Zapisuje wyselekcjonowanie elementy do bazy100
+arcpy.FeatureClassToFeatureClass_conversion('Budynek_A_layer', dane_wyjsc, 'Budynek_A')
+arcpy.FeatureClassToFeatureClass_conversion('Budynek_P_layer', dane_wyjsc, 'Budynek_P')
+arcpy.FeatureClassToFeatureClass_conversion('Magazyn_skl_layer', dane_wyjsc, 'Magazyn_skl')
+arcpy.FeatureClassToFeatureClass_conversion('Ogrodzenie_layer', dane_wyjsc, 'Ogrodzenie')
+arcpy.FeatureClassToFeatureClass_conversion('Mur_ogrodze_layer', dane_wyjsc, 'Mur_ogrodze')
+arcpy.FeatureClassToFeatureClass_conversion('Park_layer', dane_wyjsc, 'Park')
+arcpy.FeatureClassToFeatureClass_conversion('Stadion_amf_A_layer', dane_wyjsc, 'Stadion_amf_A')
+arcpy.FeatureClassToFeatureClass_conversion('Stadion_amf_P_layer', dane_wyjsc, 'Stadion_amf_P')
 
-
-
-
-
-
+#Usuwa nie ?ywane warstwy wejsciowe
+arcpy.Delete_management('Budynek_P_PAL015')
+arcpy.Delete_management('Budynek_A_AAL015')
+arcpy.Delete_management('Magazyn_skl_AAM010')
+arcpy.Delete_management('Ogrodzenie_LAL070')
+arcpy.Delete_management('Mur_ogrodze_LAL260')
+arcpy.Delete_management('Park_A_AAK120')
+arcpy.Delete_management('Stadion_amf_AAK160')
+arcpy.Delete_management('Stadion_amf_PAK160')
 
 
 
