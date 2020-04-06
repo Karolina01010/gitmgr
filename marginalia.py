8161b61f07abd71509a9c138032688d6ad83326a
Karolina <zygawska.karolina@gmail.com>
2020-04-06 15:51:46 +0200

    06.04
    odsunięcie punktu od powierzchni

--- marginalia.py
+++ marginalia.py
@@ -131,9 +131,29 @@ with arcpy.da.SearchCursor(line_lyr, ['OID@','SHAPE@','FID']) as searchCursor: #
 ResolveRoadConflicts_cartography
 arcpy.ResolveBuildingConflicts
 
+budynek=r'D:\mgr\M33033CD\BAZA_M33033.gdb\budP'
+line=r'D:\mgr\M33033CD\BAZA_M33033.gdb\szosa_droga'
+
+#ODSUWA PUNKTU OD LINII
+def bearing_to_radians(bearing):
+    return math.radians((450-bearing)%360) # ZWIJANIE WIELU OBROT?W WJEDEN =   zamiana kata na radiany  (360 +90= 450 ) rusunki  zeszyt  https://stackoverflow.com/questions/9875964/python-converting-radians-to-degrees
+
+#Tworzy punkt srodkowy wielok?ta, calculate near distance and angle and join this to polygons
+
+#arcpy.Near_analysis('budynek', 'line')    # Oblicza odleg?o?? i k?t  mi?dzy obiektami wej?ciowymi a najbli?szymi obiektami w innej warstwie lub klasie obiekt?w.
+#arcpy.MakeFeatureLayer_management('budynek','budynek_layer')   #Tworzy warstw? obiekt?w z wej?ciowej klasy obiekt?w lub pliku warstwy.
 
 
-ODSUWA POWIERZCHNIE OD LINII
+#Move the polygons
+with arcpy.da.UpdateCursor(budynek,['SHAPE@X','SHAPE@Y','NEAR_DIST','NEAR_ANGLE']) as cursor:
+    for row in cursor:
+        newx=row[0]+(row[2]-desired_distance)*math.sin(bearing_to_radians(row[3])) # oblicznie nowej wspolrzednej  Xnew = X + ( odleg?os + wartosc przesuniecia) * sin ( azymut k?ta)
+        newy=row[1]+(row[2]-desired_distance)*math.cos(bearing_to_radians(row[3])) # oblicznie nowej wspolrzednej Ynew = Y + ( odleg?os + wartosc przesuniecia) * cos ( azymut k?ta)
+        row[0]=newx
+        row[1]=newy
+        cursor.updateRow(row)
+
+#ODSUWA POWIERZCHNIE OD LINII
 
 import arcpy,math
 
@@ -158,7 +178,7 @@ arcpy.AddJoin_management(in_layer_or_view='polygon_lyr', in_field='OBJECTID', jo
 arcpy.CopyFeatures_management(in_features='polygon_lyr', out_feature_class=output_polygon_fc)
 
 #Move the polygons
-with arcpy.da.UpdateCursor(output_polygon_fc,['SHAPE@X','SHAPE@Y','points_NEAR_DIST','points_NEAR_ANGLE']) as cursor:
+with arcpy.da.UpdateCursor(output_polygon_fc,['SHAPE@X','SHAPE@Y','NEAR_DIST','NEAR_ANGLE']) as cursor:
     for row in cursor:
         newx=row[0]+(row[2]-desired_distance)*math.sin(bearing_to_radians(row[3]))
         newy=row[1]+(row[2]-desired_distance)*math.cos(bearing_to_radians(row[3]))
