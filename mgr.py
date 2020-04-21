8c32a056d73e836302b316be11c8b581ab9d9a80
Karolina <zygawska.karolina@gmail.com>
2020-04-21 16:13:22 +0200

    21.04
    Nowy plik- Praca mgr

--- mgr.py
+++ mgr.py
@@ -1,15 +1,13 @@
 import os
 import arcpy
-#1
-arcpy.CreateFileGDB_management("D:\mgr\M33033CD", "BAZA_2") # tworzymy baze
-dane_wyjsc = "D:\mgr\M33033CD\BAZA_2.gdb"                    # definiujemy ta baze
+
+arcpy.CreateFileGDB_management("D:\mgr\M33033CD", "BAZA_23") # tworzymy baze
+dane_wyjsc = "D:\mgr\M33033CD\BAZA_23.gdb"                    # definiujemy ta baze
 arcpy.env.workspace = dane_wyjsc
 
-#DODATKOWE
 #Definiuje uk?ad wspolrzednych
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("ETRS 1989 UTM Zone 33N")
 
-#2
 #Selekcja obiektow
 inFeatures = ['D:\mgr\SHP_33033CD\Budynek_P_PAL015.shp',
  'D:\mgr\SHP_33033CD\Budynek_A_AAL015.shp',
@@ -30,15 +28,10 @@ inFeatures = ['D:\mgr\SHP_33033CD\Budynek_P_PAL015.shp',
  'D:\mgr\SHP_33033CD\Teren_zabud_AAL020.shp',
  'D:\mgr\SHP_33033CD\Roslinnosc_AEB010.shp',
  'D:\mgr\SHP_33033CD\Rezerwat_pr_AAL005.shp']
-
 outLocation = dane_wyjsc
-#3
+
 #zapisuje wybrane elementy w bazie (dodawane sa tebele z wartosciami dlugosci i powierzchni obiektu)
 arcpy.FeatureClassToGeodatabase_conversion(inFeatures, outLocation)
-
-#4
-# Tworzy warstwe obiektow
-#arcpy.CalculateField_management('Budynek_P_layer',"GMRotation",'!RCD!',"PYTHON") # oclicza w kat obrotu punktu (string ->double)
 
 def MakeFeatureLayer():
     arcpy.MakeFeatureLayer_management('Budynek_P_PAL015', 'Budynek_P_layer')
@@ -60,12 +53,8 @@ def MakeFeatureLayer():
     arcpy.MakeFeatureLayer_management('Teren_zabud_AAL020', 'Teren_zabud_A_layer')
     arcpy.MakeFeatureLayer_management('Roslinnosc_AEB010', 'Roslinnosc_A_layer')
     arcpy.MakeFeatureLayer_management('Rezerwat_pr_AAL005', 'Rezerwat_pr_A_layer')
-
 MakeFeatureLayer()
 
-
-
-#5
 #Selekcjonuje obiekty ktore nastepnie sa zamieniane na punktowe
 
 def FeatureToPoint():
@@ -81,14 +70,12 @@ def FeatureToPoint():
 
     arcpy.SelectLayerByAttribute_management('Sad_plantac_A_layer', "ADD_TO_SELECTION",' "Shape_Area" < 110000 ')
     arcpy.FeatureToPoint_management('Sad_plantac_A_layer', 'Drzewo_PP')
-
 FeatureToPoint()
-#6
+
 #Selekcja po atrybutach
 
 def SelekcjaPoAtrybutach():
     arcpy.SelectLayerByAttribute_management('Budynek_A_layer', "NEW_SELECTION",' "Shape_Area" > 5400 ')
-    arcpy.FeatureClassToFeatureClass_conversion('Budynek_A_layer', dane_wyjsc, 'Budynek_A')
     arcpy.SelectLayerByAttribute_management('Magazyn_skl_A_layer', "NEW_SELECTION",' "Shape_Area" > 10000 ')
 
     arcpy.SelectLayerByAttribute_management('Park_A_layer', "NEW_SELECTION",' "Shape_Area" > 30000 ')
@@ -106,14 +93,11 @@ def SelekcjaPoAtrybutach():
     arcpy.SelectLayerByAttribute_management('Rzeka_strum_L_layer', "NEW_SELECTION", '"HAT" = \'      500\' ')
     arcpy.SelectLayerByAttribute_management('Rzeka_strum_L_layer', "ADD_TO_SELECTION",' "Shape_Length" > 500 ')
     arcpy.SelectLayerByAttribute_management('Rzeka_strum_L_layer', "ADD_TO_SELECTION", '"HAT" = \'      501\' ')
-
 SelekcjaPoAtrybutach()
 
-#7
 #Zapisuje wyselekcjonowanie elementy do bazy100
-
 def FeatureClassToFeatureClass():
-    arcpy.FeatureClassToFeatureClass_conversion('Budynek_A_layer', dane_wyjsc, 'Budynek_A')
+    arcpy.FeatureClassToFeatureClass_conversion('Budynek_A_layer', dane_wyjsc, 'Budynek_A_select')
     arcpy.FeatureClassToFeatureClass_conversion('Magazyn_skl_A_layer', dane_wyjsc, 'Magazyn_skl_A')
     arcpy.FeatureClassToFeatureClass_conversion('Ogrodzenie_L_layer', dane_wyjsc, 'Ogrodzenie_L')
     arcpy.FeatureClassToFeatureClass_conversion('Mur_ogrodze_L_layer', dane_wyjsc, 'Mur_ogrodze_L')
@@ -132,40 +116,106 @@ def FeatureClassToFeatureClass():
     arcpy.FeatureClassToFeatureClass_conversion('Roslinnosc_A_layer', dane_wyjsc, 'Roslinnosc_A')
     arcpy.FeatureClassToFeatureClass_conversion('Rezerwat_pr_A_layer', dane_wyjsc, 'Rezerwat_pr_A')
     arcpy.FeatureClassToFeatureClass_conversion('Drzewo_P_layer', dane_wyjsc, 'Drzewo_P')
-
 FeatureClassToFeatureClass()
 
-#8
-#Selekcja obiektow do stowrzenia nowych klas
-
+#klasyfikacja budynkow
 def KlasyfikacjaBudynkow():
-    arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"BFC" = \'27\' ')
-    arcpy.CopyFeatures_management('Budynek_P_layer',"Stacja_kol_P")
+    arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"EXS" = \'7\' ')
+    arcpy.CopyFeatures_management('Budynek_P_layer',"Ruiny_P1")
     arcpy.DeleteFeatures_management('Budynek_P_layer')
 
-    arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"BFC" = \'50\' ')
-    arcpy.CopyFeatures_management('Budynek_P_layer',"Kosciol_swiatynia_P")
+    arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"BFC" = \'27\' ')
+    arcpy.CopyFeatures_management('Budynek_P_layer',"Stacja_kol_P1")
+    arcpy.DeleteFeatures_management('Budynek_P_layer')
+
+    arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"HWT"=\'3\'  AND "BFC"=\'7\' AND "EXS"=\'28\' ')
+    arcpy.CopyFeatures_management('Budynek_P_layer',"Kaplica_P1")
     arcpy.DeleteFeatures_management('Budynek_P_layer')
 
     arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"BFC" = \'54\' ')
-    arcpy.CopyFeatures_management('Budynek_P_layer',"Stacja_benz_P")
+    arcpy.CopyFeatures_management('Budynek_P_layer',"Stacja_benz_P1")
     arcpy.DeleteFeatures_management('Budynek_P_layer')
 
     arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"BFC" = \'601\' ')
-    arcpy.CopyFeatures_management('Budynek_P_layer',"Przystanek")
+    arcpy.CopyFeatures_management('Budynek_P_layer',"Przystanek1")
     arcpy.DeleteFeatures_management('Budynek_P_layer')
 
-    arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"BFC" = \'7\' ')
-    arcpy.CopyFeatures_management('Budynek_P_layer',"Obiekt_kut_P")
+    arcpy.SelectLayerByAttribute_management('Budynek_P_layer', "NEW_SELECTION", '"LMC" = \'1\' AND  "BFC" <> \'999\'')  # selekcja obiektow o zanacznieu orientacyjnym LMC =1 funkcja na ko?cu ze wzgledu na to ze obiekty religijne maja LMC =1
+    arcpy.CopyFeatures_management('Budynek_P_layer',"Budynek_ZO1")
     arcpy.DeleteFeatures_management('Budynek_P_layer')
-
-    arcpy.FeatureClassToFeatureClass_conversion('Budynek_P_layer', dane_wyjsc, 'Budynek_P')
-
 KlasyfikacjaBudynkow()
 
+budynek_P = 'Budynek_P_layer'
+droga = 'Szosa_droga_L_layer'
+budynek_A = 'Budynek_A_select'
+
+def bearing_to_radians(bearing):
+        return math.radians((450-bearing)%360)
+
+# ODSUWANIE POWIERZCHNI OD LINI
+def Pow_od_lini():
+    desired_distance1 = 68
+    temp_points=r'in_memory\points'
+    #Tworzy punkt srodkowy wielok?ta, calculate near distance and angle and join this to polygons
+    arcpy.FeatureToPoint_management(budynek_A,'temp_points')   #Tworzy klas? obiekt?w zawieraj?c? punkty wygenerowane z reprezentatywnych lokalizacji obiekt?w wej?ciowych.
+    arcpy.Near_analysis('temp_points', droga,"","NO_LOCATION","ANGLE","PLANAR")   # Oblicza odleg?o?? i k?t  mi?dzy obiektami wej?ciowymi a najbli?szymi obiektami w innej warstwie lub klasie obiekt?w.
+    arcpy.MakeFeatureLayer_management(budynek_A,'polygon_lyr')   #Tworzy warstw? obiekt?w z wej?ciowej klasy obiekt?w lub pliku warstwy.
+    arcpy.AddJoin_management('polygon_lyr','OBJECTID', 'temp_points', 'OBJECTID')  #??czy warstw? z inn? warstw? lub tabel? na podstawie wsp?lnego pola (dodaje do nowej warstwy  Budynek A informacje o odleg?osci do najblizszej drogi "NEAR_DIST"  z warstwy punktowej )
+    #arcpy.CopyFeatures_management('polygon_lyr',budynek) #Kopiuje obiekty z wej?ciowej klasy obiekt?w lub warstwy do nowej klasy obiekt?w.
+    arcpy.FeatureClassToFeatureClass_conversion('polygon_lyr', dane_wyjsc, 'Budynek_A')
+    #Move the polygons
+    with arcpy.da.UpdateCursor('Budynek_A',['SHAPE@X','SHAPE@Y','temp_points_NEAR_DIST','temp_points_NEAR_ANGLE']) as cursor:
+        for row in cursor:
+            xnew=row[0]-(row[2]-desired_distance1)*math.sin(bearing_to_radians(row[3]))
+            ynew=row[1]-(row[2]-desired_distance1)*math.cos(bearing_to_radians(row[3]))
+            row[0]=xnew
+            row[1]=ynew
+            cursor.updateRow(row)
+    #arcpy.Delete_management('temp_points')
+Pow_od_lini()
+
+#odsuniecie budynkow od dorgi
+def OdsuniecieBudynkow():
+
+    arcpy.Near_analysis(budynek_P,droga,"","NO_LOCATION","ANGLE","PLANAR")    # Oblicza odleg?o?? i k?t  mi?dzy obiektami wej?ciowymi a najbli?szymi obiektami w innej warstwie lub klasie obiekt?w.
+
+    desired_distance = 78  #odleglosc dobrego widzenia + polowa przekatnej
+
+    #Move the point
+    with arcpy.da.UpdateCursor(budynek_P,['SHAPE@X','SHAPE@Y','NEAR_DIST','NEAR_ANGLE']) as cursor:
+        for row in cursor:
+            newx=row[0]+(row[2]-desired_distance)*math.sin(bearing_to_radians(row[3]))
+            newy=row[1]+(row[2]-desired_distance)*math.cos(bearing_to_radians(row[3]))
+            row[0]=newx
+            row[1]=newy
+            cursor.updateRow(row)
+OdsuniecieBudynkow()
+
+# usuwanie budynkow zbyt blisko siebie
+def NearAnalysisBudynki():
+
+    #coblicza odleglosc miedzy budynkami
+    arcpy.Near_analysis(budynek_P,budynek_P)
+
+    # powtarza dla wszystkich funkcji, kt?re znajduj? si? w odleg?o?ci 50
+    cur = arcpy.UpdateCursor(budynek_P, '"NEAR_DIST" < 58') # polowa przekoatnej 90x60
+    row1 = cur.next()  # zwraca nast?pny wiersz wej?ciowy
+    while row1:
+        cur.deleteRow(row1)  #ten punkt znajduje si? w odleg?o?ci  mniej niz 40 od s?siada, wi?c usuwa go
+        arcpy.Near_analysis(budynek_P,budynek_P) # jeszcze raz oblicza i aktualizuje odleg?oglosci aby nie usunac punktu bliskiego ktory juz nie istenieje
+
+        del row1, cur   # teraz ponownie uruchom t? procedur? w nowym zestawie danych
+        cur = arcpy.UpdateCursor(budynek_P, '"NEAR_DIST" < 58')
+        arcpy.Near_analysis(budynek_P,budynek_P) # jeszcze raz oblicza i aktualizuje odleg?oglosci aby nie usunac punktu bliskiego ktory juz nie istenieje
+        row1 = cur.next()
+        NearAnalysisBudynki
+
+        arcpy.FeatureClassToFeatureClass_conversion('Budynek_P_layer', dane_wyjsc, 'Budynek_P')
+#wywo?uje procedur? rekurencyjn?. B?dzie dzia?a? stopniowo szybciej, poniewa? za ka?dym razem b?dzie przechodzi? przez mniejsz? liczb? punkt?w
+NearAnalysisBudynki()
+
+
 # Podzia? drogi na klasy
-
-
 def KlasyfikacjaDrog():
     arcpy.SelectLayerByAttribute_management('Szosa_droga_L_layer', "NEW_SELECTION", '"TUC"=\'2\' AND "RTT"=\'16\'')
     arcpy.CopyFeatures_management('Szosa_droga_L_layer',"ZL_101")
@@ -190,10 +240,9 @@ def KlasyfikacjaDrog():
 
     arcpy.SelectLayerByAttribute_management('Szosa_droga_L_layer', "NEW_SELECTION", '"TUC"=\'501\' ')
     arcpy.CopyFeatures_management('Szosa_droga_L_layer',"ZL_018")
-
 KlasyfikacjaDrog()
 
-
+#generalizacja drogi
 def Generalizacja_drog():
     arcpy.Dissolve_management("ZL_101","D:\mgr\M33033CD\droga\ZL101.shp") # Agreguje warstw? w oparciu o okre?lone atrybuty.
     arcpy.Integrate_management("D:\mgr\M33033CD\droga\ZL101.shp",20)     #integralno?ci wsp?lnych granic element?w znjaduj?cej sie w odleglosci 15
@@ -218,11 +267,18 @@ def Generalizacja_drog():
 
     arcpy.Dissolve_management("ZL_018","D:\mgr\M33033CD\droga\ZL018.shp")
     arcpy.Integrate_management("D:\mgr\M33033CD\droga\ZL018.shp",20)
-
 Generalizacja_drog()
-#zapisuje wybrane elementy w bazie (dodawane sa tebele z wartosciami dlugosci i powierzchni obiektu)
 
-def DodanieDoBazyDrog():
+def Wygladzenie_lini_pow():
+    arcpy.Dissolve_management("Rzeka_strum_L","D:\mgr\M33033CD\Rzeka.shp") # polaczenie w jedno aby kolejna funkcja dzialala
+    arcpy.SimplifyLine_cartography("D:\mgr\M33033CD\Rzeka.shp","D:\mgr\M33033CD\Rzeka_L.shp","BEND_SIMPLIFY",200)
+
+    arcpy.SimplifyPolygon_cartography("Las_A","D:\mgr\M33033CD\Las.shp","BEND_SIMPLIFY",200)
+
+Wygladzenie_lini_pow()
+
+#zapisuje wybrane elementy w bazie (dodawane sa tebele z wartosciami dlugosci i powierzchni obiektu)
+def DodanieDoBazywarstwzshp():
     inFeaturesdrogi = ['D:\mgr\M33033CD\droga\ZL101.shp',
     'D:\mgr\M33033CD\droga\ZL103.shp',
     'D:\mgr\M33033CD\droga\ZL104.shp',
@@ -230,11 +286,15 @@ def DodanieDoBazyDrog():
     'D:\mgr\M33033CD\droga\ZL106.shp',
     'D:\mgr\M33033CD\droga\ZL107.shp',
     'D:\mgr\M33033CD\droga\ZL108.shp',
-    'D:\mgr\M33033CD\droga\ZL018.shp']
+    'D:\mgr\M33033CD\droga\ZL018.shp',
+    'D:\mgr\M33033CD\Rzeka_L.shp',
+    'D:\mgr\M33033CD\Las.shp'
+    ]
     arcpy.FeatureClassToGeodatabase_conversion(inFeaturesdrogi, outLocation)
+DodanieDoBazywarstwzsh()
+arcpy.Delete_management('Szosa_droga_L')
 
-DodanieDoBazyDrog()
-#arcpy.Delete_management('Szosa_droga_L')
+
 
 
 #Usuwa nie ?ywane warstwy wejsciowe
@@ -267,31 +327,10 @@ def UsuwanieWarstw():
     arcpy.Delete_management('ZL_107')
     arcpy.Delete_management('ZL_108')
     arcpy.Delete_management('ZL_018')
+    arcpy.Delete_management('Rzeka_strum_L')
+    arcpy.Delete_management('Las_A')
 
 UsuwanieWarstw()
-
-feature = 'Budynek_P'
-
-def NearAnalysisBudynki():
-    #coblicza odleglosc
-    arcpy.Near_analysis(feature, feature)
-
-    # powtarza dla wszystkich funkcji, kt?re znajduj? si? w odleg?o?ci 40
-    cur = arcpy.UpdateCursor(feature, '"NEAR_DIST" < 40')
-    row1 = cur.next()  # zwraca nast?pny wiersz wej?ciowy
-    while row1:
-        cur.deleteRow(row1)  #ten punkt znajduje si? w odleg?o?ci  mniej niz 40 od s?siada, wi?c usuwa go
-
-
-        del row1, cur   # teraz ponownie uruchom t? procedur? w nowym zestawie danych
-        cur = arcpy.UpdateCursor(feature, '"NEAR_DIST" < 40')
-        row1 = cur.next()
-        NearAnalysisBudynki
-
-#wywo?uje procedur? rekurencyjn?. B?dzie dzia?a? stopniowo szybciej, poniewa? za ka?dym razem b?dzie przechodzi? przez mniejsz? liczb? punkt?w
-NearAnalysisBudynki()
-
-
 
 
 
